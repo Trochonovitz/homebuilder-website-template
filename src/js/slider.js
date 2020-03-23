55b5c0c4430dc807c5ddef9fff078358e00b8b3c
Trochonovitz <mat.trochonowicz@gmail.com>
2020-03-23 11:41:57 +0100

    done home page without sliders and gallery

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,38 +1,55 @@
-const slider = document.querySelector(".slider");
-const sliderTitle = document.querySelector(".text h1");
-const sliderParagraph = document.querySelector(".text p");
-const dots = [...document.querySelectorAll(".dot")];
+import Swiper from "swiper";
 
-let index = 0;
-let time = 4000;
+const slider = document.querySelector(".swiper-container");
 
-const sliderProps = {
-  img: ["/img/bg_0.jpg", "/img/bg_1.jpg", "/img/bg_2.jpg"],
-  text: [
-    "WE'RE HERE TO HELP YOU",
-    "BEST BUILDER IN THE WORLD",
-    "WE BUILD YOUR HOME"
-  ],
-  title: ["HOME BUILDER", "WE BUILD HOME", "PROFESSIONAL BUILDER"]
-};
+const mySwiper = new Swiper(slider, {
+  pagination: {
+    el: ".swiper-pagination"
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  },
+  slidesPerView: 2,
+  spaceBetween: 30
+});
 
-const changeSlide = index => {
-  slider.style.backgroundImage = `url(${sliderProps.img[index]})`;
-  sliderTitle.textContent = sliderProps.title[index];
-  sliderParagraph.textContent = sliderProps.text[index];
-};
+// mySwiper.navigation.nextEl.addEventListener("click", console.log("costam"));
 
-const addActiveDot = index => {
-  dots.forEach(dot => dot.classList.remove("dot--active"));
+// const sliderTitle = document.querySelector(".text h1");
+// const sliderParagraph = document.querySelector(".text p");
+// const dots = [...document.querySelectorAll(".dot")];
 
-  for (let i = 0; i < dots.length; i++) {
-    dots[index].classList.add("dot--active");
-  }
-};
+// let index = 0;
+// let time = 4000;
 
-setInterval(() => {
-  index >= 2 ? (index = 0) : index++;
+// const sliderProps = {
+//   img: ["/img/bg_0.jpg", "/img/bg_1.jpg", "/img/bg_2.jpg"],
+//   text: [
+//     "WE'RE HERE TO HELP YOU",
+//     "BEST BUILDER IN THE WORLD",
+//     "WE BUILD YOUR HOME"
+//   ],
+//   title: ["HOME BUILDER", "WE BUILD HOME", "PROFESSIONAL BUILDER"]
+// };
 
-  changeSlide(index);
-  addActiveDot(index);
-}, time);
+// const changeSlide = index => {
+//   slider.style.backgroundImage = `url(${sliderProps.img[index]})`;
+//   sliderTitle.textContent = sliderProps.title[index];
+//   sliderParagraph.textContent = sliderProps.text[index];
+// };
+
+// const addActiveDot = index => {
+//   dots.forEach(dot => dot.classList.remove("dot--active"));
+
+//   for (let i = 0; i < dots.length; i++) {
+//     dots[index].classList.add("dot--active");
+//   }
+// };
+
+// setInterval(() => {
+//   index >= 2 ? (index = 0) : index++;
+
+//   changeSlide(index);
+//   addActiveDot(index);
+// }, time);
