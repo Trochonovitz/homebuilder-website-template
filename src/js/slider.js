66ca34681cced76600ee2fc1d92a0db6c73e9f29
Trochonovitz <mat.trochonowicz@gmail.com>
2020-03-23 12:11:26 +0100

    slider issue

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,51 +1,35 @@
-import Swiper from "swiper";
-
 const slider = document.querySelector(".swiper-container");
 
-const mySwiper = new Swiper(slider, {
-  pagination: {
-    el: ".swiper-pagination"
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev"
-  },
-  slidesPerView: 2,
-  spaceBetween: 30
-});
+const sliderTitle = document.querySelector(".text h1");
+const sliderParagraph = document.querySelector(".text p");
+const dots = [...document.querySelectorAll(".dot")];
 
-// mySwiper.navigation.nextEl.addEventListener("click", console.log("costam"));
+let index = 0;
+let time = 4000;
 
-// const sliderTitle = document.querySelector(".text h1");
-// const sliderParagraph = document.querySelector(".text p");
-// const dots = [...document.querySelectorAll(".dot")];
+const sliderProps = {
+  img: ["/img/bg_0.jpg", "/img/bg_1.jpg", "/img/bg_2.jpg"],
+  text: [
+    "WE'RE HERE TO HELP YOU",
+    "BEST BUILDER IN THE WORLD",
+    "WE BUILD YOUR HOME"
+  ],
+  title: ["HOME BUILDER", "WE BUILD HOME", "PROFESSIONAL BUILDER"]
+};
 
-// let index = 0;
-// let time = 4000;
+const changeSlide = index => {
+  slider.style.backgroundImage = `url(${sliderProps.img[index]})`;
+  sliderTitle.textContent = sliderProps.title[index];
+  sliderParagraph.textContent = sliderProps.text[index];
+};
 
-// const sliderProps = {
-//   img: ["/img/bg_0.jpg", "/img/bg_1.jpg", "/img/bg_2.jpg"],
-//   text: [
-//     "WE'RE HERE TO HELP YOU",
-//     "BEST BUILDER IN THE WORLD",
-//     "WE BUILD YOUR HOME"
-//   ],
-//   title: ["HOME BUILDER", "WE BUILD HOME", "PROFESSIONAL BUILDER"]
-// };
+const addActiveDot = index => {
+  dots.forEach(dot => dot.classList.remove("dot--active"));
 
-// const changeSlide = index => {
-//   slider.style.backgroundImage = `url(${sliderProps.img[index]})`;
-//   sliderTitle.textContent = sliderProps.title[index];
-//   sliderParagraph.textContent = sliderProps.text[index];
-// };
-
-// const addActiveDot = index => {
-//   dots.forEach(dot => dot.classList.remove("dot--active"));
-
-//   for (let i = 0; i < dots.length; i++) {
-//     dots[index].classList.add("dot--active");
-//   }
-// };
+  for (let i = 0; i < dots.length; i++) {
+    dots[index].classList.add("dot--active");
+  }
+};
 
 // setInterval(() => {
 //   index >= 2 ? (index = 0) : index++;
